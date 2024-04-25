4c1bda81015e42f3d8ce3f892d149a1fa11fbb07
alwex0920 <147579253+alwex0920@users.noreply.github.com>
2024-04-25 11:31:27 +0800

    Delete citata.py

--- citata.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-def citata_gen():
-    response = requests.get("https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=ru")
-    data = response.json()
-    citata = data["quoteText"]
-    return citata
